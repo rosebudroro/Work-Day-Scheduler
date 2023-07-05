63274c71971ad8e9ccd8c673f1eb05f201fdc8b1
Roxy Osorio <roxy.osorio@moo.com>
2023-07-04 21:02:43 -0400

    local storage

--- Develop/script.js
+++ Develop/script.js
@@ -26,3 +26,24 @@ var today = dayjs();
 $('#currentDay').text(today.format('MMM D, YYYY'));
 var time = dayjs().format('hh:mm:ss');
 $('#currentTime').text(time);
+
+
+$('.saveBtn').on('click', function () {
+  var description = $(this).siblings('.description').val();
+  var timeSlot = $(this).parent().attr('id');
+
+  //local storage
+  localStorage.setItem(timeSlot, description);
+});
+
+
+$('#hour-8 .description').val(localStorage.getItem('hour-8'));
+$('#hour-9 .description').val(localStorage.getItem('hour-9'));
+$('#hour-10 .description').val(localStorage.getItem('hour-10'));
+$('#hour-11 .description').val(localStorage.getItem('hour-11'));
+$('#hour-12 .description').val(localStorage.getItem('hour-12'));
+$('#hour-13 .description').val(localStorage.getItem('hour-13'));
+$('#hour-14 .description').val(localStorage.getItem('hour-14'));
+$('#hour-15 .description').val(localStorage.getItem('hour-15'));
+$('#hour-16 .description').val(localStorage.getItem('hour-16'));
+$('#hour-17 .description').val(localStorage.getItem('hour-17'));
